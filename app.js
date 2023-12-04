f540a2b68a7a386ad4efbe384ff5e827bb84dbfc
shreyas bhaskar <bhaskar.s@northeastern.edu>
2023-12-04 16:35:19 -0500

    Eneno jeevana

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-import "dotenv/config";
-import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import express from 'express';
+import Hello from "./hello.js"
+import Lab5 from './lab5.js';
 import cors from "cors";
-import CourseRoutes from "./Courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import "dotenv/config";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from './users/routes.js';
+import session from "express-session";
+
+console.log(process.env.DB_CONNECTION_STRING);
+const CONNECTION_STRING = 'mongodb+srv://shreyasb63:admin@kanbas.kf9yoku.mongodb.net/kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(
+    cors({
+        credentials: false,
+        origin: '*'
+    })
+);
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
